0dccab79f8d397809fe41bf0db66f9ee9711e6ad
zolar <zolars@outlook.com>
2017-09-06 22:54:32 +0800

    2017-09-06 22:54:30

---
+++ Code/Maximum Subarray.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = nums[0]
+        for i in range(len(nums))[1:]:
+            nums[i] = max(nums[i] + nums[i - 1], nums[i])
+            ans = max(ans, nums[i])
+
+        print nums
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution().maxSubArray([1, -2]))
