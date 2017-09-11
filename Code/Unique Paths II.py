e86277751318d574109ab0d20f613632e78ea2fd
zolar <zolars@outlook.com>
2017-09-11 10:18:25 +0800

    2017-09-11 10:17:51
    delete some rubbish

--- Code/Unique Paths II.py
+++ Code/Unique Paths II.py
@@ -10,15 +10,12 @@ class Solution(object):
         dp = []
         for i in range(len(obstacleGrid)):
             for j in range(len(obstacleGrid[i])):
-                print i, j, obstacleGrid[i][j]
                 if obstacleGrid[i][j]:
                     obstacleGrid[i][j] = -1
             dp.append([0] + obstacleGrid[i])
 
         dp = [[0] * (len(obstacleGrid[0]) + 1)] + dp
         dp[0][1] = 1
-
-        print dp
 
         for i in range(1, len(dp)):
             for j in range(1, len(dp[i])):
@@ -28,7 +25,6 @@ class Solution(object):
                 if dp[i][j] < 0:
                     dp[i][j] = 0
 
-        print dp
         return dp[-1][-1]
 
 
